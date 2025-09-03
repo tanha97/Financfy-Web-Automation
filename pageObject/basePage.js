ecc7977ed8b3960f6d3b77576d34a6ed7bbb0678
tanha97 <137722129+tanha97@users.noreply.github.com>
2025-09-03 17:36:37 +0600

    fix baseURL issue

--- pageObject/basePage.js
+++ pageObject/basePage.js
@@ -1,10 +1,11 @@
 export class BasePage {
-  constructor(page) {
+  constructor(page, baseURL) {
     this.page = page;
+    this.baseURL= baseURL
   }
 
   // Navigation
-  async goto(url='/') {
+  async goto(path='/') {
     const url = this.baseURL ? new URL(path, this.baseURL).toString() : path;
     await this.page.goto(url);
   }
