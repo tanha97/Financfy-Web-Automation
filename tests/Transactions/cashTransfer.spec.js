c005bd18e1636590f6b8596247a6efe77ea45e62
tanha97 <137722129+tanha97@users.noreply.github.com>
2025-09-25 12:57:03 +0600

    run workflow

--- tests/Transactions/cashTransfer.spec.js
+++ tests/Transactions/cashTransfer.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '../../pageObject/baseTest.js'
 
-test.only('Cash Transfer create', async ({ cashTransferPage }) => {
+test('Cash Transfer create', async ({ cashTransferPage }) => {
   await cashTransferPage.createCashTransfer('1000', 'Transfer Done')
   const toastText = await cashTransferPage.getToastText(5000)
   await expect(toastText).toContain('Successfully transferred cash.')
