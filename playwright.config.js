d364203446905888c29ae86599ad47bcf9abdc4d
tanha97 <137722129+tanha97@users.noreply.github.com>
2025-10-26 23:19:30 +0600

    add playwrite agent

--- playwright.config.js
+++ playwright.config.js
@@ -1,81 +1,64 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
 
-import { defineConfig, devices } from '@playwright/test'
-import * as dotenv from 'dotenv'
-dotenv.config()
-export const ENV = process.env.ENV?.toLowerCase()  || 'staging'
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-const configMap = {
-  staging: {
-    baseURL: process.env.STAGING_URL,
-    mobileNumber: process.env.STAGING_MOBILE_NUMBER,
-    password: process.env.STAGING_PASSWORD,
-  },
-  production: {
-    baseURL: process.env.PROD_URL,
-    mobileNumber: process.env.PROD_MOBILE_NUMBER,
-    password: process.env.PROD_PASSWORD,
-  },
-}
-
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
 export default defineConfig({
-  testDir: './tests',
-
+  testDir: './e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
-
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  retries: 0,
-  workers: 1,
-
-  reporter: [
-    ['line'], // for console output
-    ['html', {outputFolder: 'playwright-report', open: 'never'}], // built-in HTML report
-    ['allure-playwright'], // Allure report
-  ],
-
-  globalSetup: './tests/setup/globalSetup.js',
-
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    /* Base URL to use in actions like `await page.goto('')`. */
+    // baseURL: 'http://localhost:3000',
 
-    baseURL: configMap[ENV].baseURL,
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: process.env.CI ==='true',
-    storageState: 'storageState.json',
-    testMatch: ['**/*.spec.js'], //'**/utils/*.js'
-    screenshot: 'on', // capture screenshot if test fails
-    video: 'on',
   },
-  
 
   /* Configure projects for major browsers */
-
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
 
-    //   {
-    //     name: 'firefox',
-    //     use: { ...devices['Desktop Firefox'] },
-    //   },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    //   {
-    //     name: 'webkit',
-    //     use: { ...devices['Desktop Safari'] },
-    //   },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
-    //  /* Test against mobile viewports*/
-
-    //   {
-    //      name: 'Mobile Chrome',
-    //      use: { ...devices['Pixel 5'] },
-    //    },
-    //    {
-    //      name: 'Mobile Safari',
-    //      use: { ...devices['iPhone 12'] },
-    //    },
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
 
     /* Test against branded browsers. */
     // {
@@ -85,7 +68,7 @@ export default defineConfig({
     // {
     //   name: 'Google Chrome',
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    //},
+    // },
   ],
 
   /* Run your local dev server before starting the tests */
@@ -94,5 +77,5 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+});
 
-})
